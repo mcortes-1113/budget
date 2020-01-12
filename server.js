886f07de1ebfd85b465de85af0f26450a778bfae
Mario Cortes <mcortes.1113@gmail.com>
2020-01-11 23:45:38 -0600

    Added html home route

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
+const path = require("path");
 
-const PORT = 3000;
+const PORT = 5000;
 
 const app = express();
 
@@ -21,6 +22,11 @@ mongoose.connect("mongodb://localhost/budget", {
 });
 
 // routes here
+
+app.get("/", function (req, res) {
+    console.log("home route");
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+});
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
