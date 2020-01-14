fa9483c5e825867d969cb4b9f3cc74b809d6f4a0
Mario Cortes <mcortes.1113@gmail.com>
2020-01-13 22:20:23 -0600

    Added ajax code to post transactions

--- public/app.js
+++ public/app.js
@@ -1,32 +1,71 @@
-const calculations = require("./calculations");
-
-const priceEl = document.getElementById("price");
+// const calculations = require("./calculations");
+// import calculations from "./calculations"
+// import { subtract, add } from "./calculations";
+// const transactions = require("../models/transactions");
+const amountEl = document.getElementById("amount");
 const balanceEl = document.getElementById("balance");
-const expenseEl = document.getElementById("expense");
+const itemEl = document.getElementById("item");
 const expensesListEl = document.getElementById("expenses-list");
 const expenseBtn = document.getElementById("expenseBtn");
 const depositBtn = document.getElementById("depositBtn");
-const resetBtn = document.getElementById("reset");
+const resetBtn = document.getElementById("resetBtn");
 
-function addToList(name, price) {
-  expensesListEl.innerHTML += `<li class="list-group-item">Name: ${name}
-    <span class="ml-4">Price: ${price}</span></li>`;
+//=====This line needs to be added to package.json scripts for deployment "heroku-postbuild": "webpack -p"
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function addition(a, b) {
+return a + b;
+}
+
+function addExpenseToList(name, price) {
+  expensesListEl.innerHTML += `<li class="list-group-item">Expense Description: ${name}
+    <span class="ml-4">Amount: $ ${price}</span></li>`;
+}
+
+function addDepositToList(name, price) {
+  expensesListEl.innerHTML += `<li class="list-group-item">Deposit Description: ${name}
+    <span class="ml-4">Amount: $ ${price}</span></li>`;
 }
 
 function expense(e) {
   e.preventDefault();
-  console.log("expense button is working");
-  const total = calculations.subtract(Number(balanceEl.innerText), priceEl.value);
-  balanceEl.innerText = total;
-  addToList(expenseEl.value, priceEl.value);
-}
+  const currentBal = Number(balanceEl.innerText);
+  const entryAmt = Number(amountEl.value);
+  const balanceAfterExpense = subtract(currentBal, entryAmt );
+  const item =itemEl.value
 
-function deposit(e) {
-  e.preventDefault();
-  console.log("deposit button is working");
-  const total = calculations.add(Number(balanceEl.innerText), priceEl.value);
-  balanceEl.innerText = total;
-  addToList(expenseEl.value, priceEl.value);
+    balanceEl.innerText = balanceAfterExpense;
+    addExpenseToList(item, entryAmt);
+    data = {item: item, amount: entryAmt, newBalance: balanceAfterExpense};
+    $.ajax({url: "api/Transactions", type: "POST", data})
+      .done(function(response) {
+        console.log(response);
+      })
+        .fail(function (err) {
+          console.log(err)
+        })
+  };
+
+function deposit(d) {
+  d.preventDefault();
+  const currentBal = Number(balanceEl.innerText);
+  const entryAmt = Number(amountEl.value);
+  const balanceAfterDeposit = addition(currentBal, entryAmt);
+  const item =itemEl.value
+    
+    balanceEl.innerText = balanceAfterDeposit;
+    addDepositToList(item, entryAmt);
+    data = {item: item, amount: entryAmt, newBalance: balanceAfterDeposit};
+    $.ajax({url: "api/Transactions", type: "POST", data})
+      .done(function(response) {
+        console.log(response);
+      })
+        .fail(function (err) {
+          console.log(err)
+        })
 }
 
 function reset(e) {
