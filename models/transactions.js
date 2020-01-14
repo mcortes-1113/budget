bd6581ddd1b847188fbd43d807ae3cac14a43681
Mario Cortes <mcortes.1113@gmail.com>
2020-01-13 21:56:29 -0600

    Created mongoose Transactions schema

---
+++ models/transactions.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const transactionsSchema = new Schema(
+  {
+    item: { type: String, required: true },
+    amount: { type: Number, required: true },
+    date: { type: Date, default: Date.now },
+  }
+);
+
+var Transactions = mongoose.model("Transactions", transactionsSchema);
+
+module.exports = Transactions;
