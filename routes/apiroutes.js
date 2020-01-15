b76e511b67501fe7a4b42923c33edac398dc7b25
Mario Cortes <mcortes.1113@gmail.com>
2020-01-14 20:18:19 -0600

    updated GET request

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -3,11 +3,13 @@ var Transactions = require("../models/transactions");
 module.exports = function (app) {
   app.get("/api/transactions", function (req, res) {
     Transactions.find({}).then(function (data) {
+      console.log(data);
       res.send(data);
     });
   });
 
   app.post("/api/transactions", (req, res) => {
+    console.log(req.body);
     Transactions.create(req.body)
       .then(db => {
         res.json(db);
